426e50e8498341c2e5758992dc7933ab1bf2a087
dswagh <dswagh9@gmail.com>
2022-05-30 12:42:03 +0530

    Updates

---
+++ precache-manifest.f5bfc524f5c03909008919be6b4a8e3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "78b6179a3aa97b891635",
+    "url": "/TesxtUtil/static/css/main.5022869c.chunk.css"
+  },
+  {
+    "revision": "78b6179a3aa97b891635",
+    "url": "/TesxtUtil/static/js/main.78b6179a.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/TesxtUtil/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "b649ee8760ab0a60bd8e",
+    "url": "/TesxtUtil/static/css/2.e56b036b.chunk.css"
+  },
+  {
+    "revision": "b649ee8760ab0a60bd8e",
+    "url": "/TesxtUtil/static/js/2.b649ee87.chunk.js"
+  },
+  {
+    "revision": "dbcdb29e80f179748887",
+    "url": "/TesxtUtil/static/js/runtime~main.dbcdb29e.js"
+  },
+  {
+    "revision": "14311e4131710317895dd5ec21474417",
+    "url": "/TesxtUtil/index.html"
+  }
+];
